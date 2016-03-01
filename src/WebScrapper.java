b293e9dc920d72cb67822ece98c0e7c04a20a9a6
xulegaspi <xulegaspi@gmail.com>
2016-03-01 14:22:03 +0100

    Warnings shut down.

--- src/WebScrapper.java
+++ src/WebScrapper.java
@@ -4,12 +4,21 @@
  */
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.ScriptException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.CSSParseException;
+import org.w3c.css.sac.ErrorHandler;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.logging.Level;
 
 public class WebScrapper {
 
@@ -23,10 +32,43 @@ public class WebScrapper {
         // Open the WebClient
         WebClient webClient = new WebClient();
 
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
+
+        webClient.setIncorrectnessListener((arg0, arg1) -> {
+            // TODO Auto-generated method stub
+
+        });
+        webClient.setCssErrorHandler(new ErrorHandler() {
+
+            @Override
+            public void warning(CSSParseException exception) throws CSSException {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void fatalError(CSSParseException exception) throws CSSException {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void error(CSSParseException exception) throws CSSException {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
         // Retrieve the whole web page given the URL
         HtmlPage page = webClient.getPage("http://www.morningstar.se/Funds/Quickrank.aspx?cb=on");
 
         Boolean stop = false;
+
+        int page_num = 1;
 
         // Loop to iterate until there's no "next page". Using XPath to select the "next page" button
         while (page.getByXPath("/html/body/form/div[4]/div[4]/div[2]/div[9]/table/tfoot/tr/td/a[15]") != null || !stop) {
@@ -38,7 +80,8 @@ public class WebScrapper {
             for (HtmlTableBody body : table.getBodies()) {
                 List<HtmlTableRow> rows = body.getRows();
 
-                System.out.println("NUMBER OF ROWS: " + rows.size());
+                System.out.println("=========\nPAGE N: " + page_num + "\nRows: " + rows.size() + "\n=========");
+                //System.out.println("NUMBER OF ROWS: " + rows.size());
                 if (rows.size() < 20) {
                     stop = true;
                 }
@@ -58,6 +101,8 @@ public class WebScrapper {
                 }
 
             }
+
+            page_num++;
 
             // Select the button "next"
             HtmlAnchor next = (HtmlAnchor) page.getByXPath("/html/body/form/div[4]/div[4]/div[2]/div[9]/table/tfoot/tr/td/a[25]").get(0);
